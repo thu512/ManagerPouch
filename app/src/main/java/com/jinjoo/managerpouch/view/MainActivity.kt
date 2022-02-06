1c693fdadad22cc4585c3675934ceadc36a8ad6d
Chang joo LEE <thu512@naver.com>
2022-02-06 16:23:59 +0900

    commit test

--- app/src/main/java/com/jinjoo/managerpouch/view/MainActivity.kt
+++ app/src/main/java/com/jinjoo/managerpouch/view/MainActivity.kt
@@ -18,12 +18,12 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         initNavigation()
+        //need add appbar
     }
 
     private fun initNavigation() {
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.main_fragment_container) as NavHostFragment
         val navController = navHostFragment.findNavController()
         binding.mainBottomNavigation.setupWithNavController(navController)
-
     }
 }
